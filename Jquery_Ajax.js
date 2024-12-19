3bfe27511365032f2f33895c5267b42ee3e69b59
KennethAbarca <kenneth.nene31@gmail.com>
2024-12-19 00:59:24 -0600

    Eliminacion
    Se eliminaron algunos archivos que se agregaron por error

--- Jquery_Ajax.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-    url: 'api/recetas.php',
-    method: 'GET',
-    dataType: 'json',
-    success: function(response) {
-        console.log("Recetas:", response);
-        // Podrías iterar y mostrarlas en la página
-    }
-});
